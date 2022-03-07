be8d434a5f468ec1829f3977f5a3a0de00f00213
thanhnhon14022000 <thanhnhon1402@gmail.com>
2022-03-07 15:20:32 +0700

    Cap nhat HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,7 +20,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  @override
+  @override 
   State<HomePage> createState() => _HomePageState();
 }
 
